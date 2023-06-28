34e057e8ea62c4ad129a4a789e9f3036ced4195f
syususp <syususp@yandex.ru>
2023-06-28 22:41:14 +0300

    Add email|password to user scheme

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -14,6 +15,19 @@ const userSchema = new mongoose.Schema({
     maxlength: 30,
   },
   avatar: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Invalid email format',
+    },
+  },
+  password: {
     type: String,
     required: true,
   },
