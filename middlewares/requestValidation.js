3f5e195f061d3edbcc77f99226ad787f959a6084
syususp <syususp@yandex.ru>
2023-07-01 13:21:48 +0300

    Fix #3 /signup route

--- middlewares/requestValidation.js
+++ middlewares/requestValidation.js
@@ -1,21 +1,29 @@
 const Joi = require('joi');
 const { BAD_REQUEST } = require('../constants/errorStatuses');
+const User = require('../models/user');
 
-const validateRequest = (schema) => (req, res, next) => {
+const validateRequest = (schema) => async (req, res, next) => {
   const { error } = schema.validate(req.body, { abortEarly: false });
 
   if (error) {
-    return res.status(BAD_REQUEST).json({ message: 'Ошибка валидации' });
+    return res.status(BAD_REQUEST).json({ message: 'Ошибка валидации', errors: error.details });
   }
 
   return next();
 };
 
 const userSchema = Joi.object({
-  name: Joi.string().min(2).max(30).required(),
+  name: Joi.string().min(2).max(30),
   email: Joi.string().email().required(),
   password: Joi.string().required(),
-  avatar: Joi.string().uri().required(),
+  avatar: Joi.string().uri(),
+}).custom(async (value) => {
+  const { email } = value;
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new Error('Данный email уже зарегистрирован');
+  }
+  return value;
 });
 
 const cardSchema = Joi.object({
