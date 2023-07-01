70a9f9348a9122e404fd64d01562cd9e99cae190
syususp <syususp@yandex.ru>
2023-07-01 14:28:30 +0300

    Fix #3.4 test /signup

--- app.js
+++ app.js
@@ -2,15 +2,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const { celebrate, Joi } = require('celebrate');
 const routes = require('./routes/index');
 const { NOT_FOUND } = require('./constants/errorStatuses');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  useNewUrlParser: true,
-})
+mongoose
+  .connect('mongodb://127.0.0.1:27017/mestodb', {
+    useNewUrlParser: true,
+  })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
 
@@ -20,8 +22,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
+
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+      avatar: Joi.string().uri(),
+    }),
+  }),
+  createUser,
+);
 
 app.use(auth);
 app.use(routes);
--- middlewares/requestValidation.js
+++ middlewares/requestValidation.js
@@ -1,16 +1,39 @@
-const Joi = require('joi');
+// const Joi = require('joi');
+// const { BAD_REQUEST } = require('../constants/errorStatuses');
+
+// const validateRequest = (schema) => (req, res, next) => {
+//   const { error } = schema.validate(req.body, { abortEarly: false });
+
+//   if (error) {
+//     const validationErrors = error.details.map((detail) => detail.message);
+// eslint-disable-next-line max-len
+//     return res.status(BAD_REQUEST).json({ message: 'Ошибка валидации', errors: validationErrors });
+//   }
+
+//   return next();
+// };
+
+// const userSchema = Joi.object({
+//   about: Joi.string(),
+//   name: Joi.string().min(2).max(30),
+//   email: Joi.string().email().required(),
+//   password: Joi.string().required(),
+//   avatar: Joi.string().uri(),
+// });
+
+// const cardSchema = Joi.object({
+//   name: Joi.string().required(),
+//   link: Joi.string().uri().required(),
+// });
+
+// module.exports = {
+//   validateUserRequest: validateRequest(userSchema),
+//   validateCardRequest: validateRequest(cardSchema),
+// };
+
+
+const { celebrate, Joi } = require('celebrate');
 const { BAD_REQUEST } = require('../constants/errorStatuses');
-
-const validateRequest = (schema) => (req, res, next) => {
-  const { error } = schema.validate(req.body, { abortEarly: false });
-
-  if (error) {
-    const validationErrors = error.details.map((detail) => detail.message);
-    return res.status(BAD_REQUEST).json({ message: 'Ошибка валидации', errors: validationErrors });
-  }
-
-  return next();
-};
 
 const userSchema = Joi.object({
   about: Joi.string(),
@@ -25,7 +48,10 @@ const cardSchema = Joi.object({
   link: Joi.string().uri().required(),
 });
 
+const validateUserRequest = celebrate(userSchema, { abortEarly: false });
+const validateCardRequest = celebrate(cardSchema, { abortEarly: false });
+
 module.exports = {
-  validateUserRequest: validateRequest(userSchema),
-  validateCardRequest: validateRequest(cardSchema),
+  validateUserRequest,
+  validateCardRequest,
 };
