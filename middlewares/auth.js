646cf2682ff73ca31f8d54dabc7652e31611d5e0
syususp <syususp@yandex.ru>
2023-07-01 18:16:02 +0300

    Fix #4.33

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,13 +2,20 @@ const jwt = require('jsonwebtoken');
 const { UNAUTHORIZED } = require('../constants/errorStatuses');
 
 const auth = (req, res, next) => {
-  const tokenString = req.headers.cookie;
+  const tokenString = req.headers.cookie || req.headers.authorization;
+  let token;
 
   if (!tokenString) {
     return res.status(UNAUTHORIZED).json({ message: `Ошибка авторизации: ${tokenString}` });
   }
 
-  const token = tokenString.replace('token=', '');
+  if (tokenString.startsWith('token=')) {
+    token = tokenString.replace('token=', '');
+  }
+
+  if (tokenString.startsWith('Bearer ')) {
+    token = tokenString.replace('Bearer ', '');
+  }
 
   try {
     const payload = jwt.verify(token, 'super-secret-key');
